47e602c01600656a251504899ddcd3f60d60474d
DanielMorales1103 <demurizar@muniguate.com>
2025-09-27 22:23:27 -0600

    Init y scriptableobject

---
+++ Lab4Inventory/Assets/Scripts/Gameplay/PickUpData.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+public enum PickUpKind { Score, Heal, Speed, Key, AudioLog }
+
+[CreateAssetMenu(menuName = "Data/PickUp", fileName = "NewPickUp")]
+public class PickUpData : ScriptableObject
+{
+    [Header("Identidad")]
+    public string displayName;
+    public Sprite icon;
+
+    [Header("Tipo de efecto")]
+    public PickUpKind kind;
+
+    [Header("Parámetros")]
+    public int scoreAmount;      
+    public int healAmount;        
+    public float speedSeconds;    
+    public string doorKey;        
+    public AudioClip audioClip;   
+
+    [Header("Inventario")]
+    public bool goesToInventory = true; 
+}
